c78d8f965300a8178c167be1d9fd0cde1433dc6b
aceky <tafinasoa35@gmail.com>
2025-06-23 18:23:56 +0300

    Refactor advantages section and QR code components
    Updated "Advantages" and "BlocQr" components to improve readability and reusability. Refactored props structure, added better typing, and integrated dynamic image sourcing and QR code logic. Minor styling adjustments were also made.

--- src/components/home/advantages/CardBlur.tsx
+++ src/components/home/advantages/CardBlur.tsx
@@ -1,108 +1,117 @@
 "use client";
-import { AdvantagesBloc } from "@/components/strapi-components/Advantages";
-import { BobButton } from "@/components/ui/BobButton";
-import { useTheme } from "@/context/themeContext";
-import { useRouter } from "next/navigation";
-
+import {BobButton} from "@/components/ui/BobButton";
+import {useTheme} from "@/context/themeContext";
+import {useRouter} from "next/navigation";
+import HighlightedText from "@/components/common/HighlightedText";
 
 
 interface Button {
-    buttonText: string;
-    buttonLink: string
+  buttonText: string;
+  buttonLink: string
 }
 
 interface CardBlurProps {
-    advantagesBloc: AdvantagesBloc[];
-    button: Button
+  advantagesBloc: {
+    titre: string;
+    description: string;
+  }[];
+  button: Button
 }
 
-export default function CardBlur({ advantagesBloc, button }: CardBlurProps) {
-    const { theme: currentTheme } = useTheme();
-    const router = useRouter();
+export default function CardBlur({advantagesBloc, button}: CardBlurProps) {
+  const {theme: currentTheme} = useTheme();
+  const router = useRouter();
 
-    const handleClick = () => {
-        if (button?.buttonLink && button?.buttonLink.trim() !== "") {
-            router.push(button?.buttonLink);
-        }
+  const handleClick = () => {
+    if (button?.buttonLink && button?.buttonLink.trim() !== "") {
+      router.push(button?.buttonLink);
     }
+  }
 
-    return (
-        <div className="relative w-fit flex flex-col gap-8"
-        >
-            <div className="pb-12 md:pb-8 lg:pb-10 w-fit h-fit backdrop-blur-3xl md:backdrop-blur-2xl rounded-[50px] border border-[var(--foreground)]/10 overflow-hidden">
-                <div
-                    className={`relative px-8 py-8 w-fit h-auto flex justify-center items-stretch  max-w-full rounded-[50px] bg-[var(--foreground)/10 overflow-hidden
+  return (
+      <div className="relative w-fit flex flex-col gap-8"
+      >
+        <div
+            className="pb-12 md:pb-8 lg:pb-10 w-fit h-fit backdrop-blur-3xl md:backdrop-blur-2xl rounded-[50px] border border-[var(--foreground)]/10 overflow-hidden">
+          <div
+              className={`relative px-8 py-8 w-fit h-auto flex justify-center items-stretch  max-w-full rounded-[50px] bg-[var(--foreground)/10 overflow-hidden
                     ${currentTheme === "light" ? " opacity-0" : " opacity-100"}
                     `}
-                    style={{
-                        width: "min(485px, 90vw)",
-                        filter: "blur(30px)",
-                        background: currentTheme === "dark" ? "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(15, 64, 146, 0.21) 70.27%)" : ""
-                    }}
-                >
-                    <AdvantageList advantagesBloc={advantagesBloc} isBackground={true} />
+              style={{
+                width: "min(485px, 90vw)",
+                filter: "blur(30px)",
+                background: currentTheme === "dark" ? "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(15, 64, 146, 0.21) 70.27%)" : ""
+              }}
+          >
+            <AdvantageList advantagesBloc={advantagesBloc} isBackground={true}/>
 
-                </div>
-                <div className="w-fit h-auto rounded-[50px] overflow-hidden">
-                    <AdvantageList advantagesBloc={advantagesBloc} isBackground={false} />
-                </div>
-            </div>
-            <BobButton
-                onClick={handleClick}
-                className=" px-8 w-full flex text-black justify-center"
-                style={{
-                    fontFamily: 'Inter',
-                    fontStyle: "normal",
-                    fontWeight: 600,
-                    fontSize: "16px",
-                    lineHeight: "19px",
-                }}
-            >
-                {button?.buttonText}
-            </BobButton>
+          </div>
+          <div className="w-fit h-auto rounded-[50px] overflow-hidden">
+            <AdvantageList advantagesBloc={advantagesBloc} isBackground={false}/>
+          </div>
         </div>
-    )
+        <BobButton
+            onClick={handleClick}
+            className=" px-8 w-full flex text-black justify-center"
+            style={{
+              fontFamily: 'Inter',
+              fontStyle: "normal",
+              fontWeight: 600,
+              fontSize: "16px",
+              lineHeight: "19px",
+            }}
+        >
+          {button?.buttonText}
+        </BobButton>
+      </div>
+  )
 }
 
 
-function AdvantageList({ advantagesBloc, isBackground = false }: { advantagesBloc: AdvantagesBloc[], isBackground?: boolean }) {
-    const { theme: currentTheme } = useTheme();
+function AdvantageList({advantagesBloc, isBackground = false}: {
+  advantagesBloc: {
+    description: string;
+    titre: string;
+  }[],
+  isBackground?: boolean
+}) {
+  const {theme: currentTheme} = useTheme();
 
 
-    return (
-        <ul className={` ${isBackground ? "flex flex-col gap-8 w-full h-auto text-transparent pointer-events-none select-none" : "absolute top-0 px-8 py-12  w-full h-[120%] flex flex-col gap-8 rounded-[50px] "} rounded-[50px] overflow-hidden`}
-            style={{
-                width: "min(485px, 90vw)",
-                background: currentTheme === "dark" ? "" : "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(15, 64, 146, 0.21) 70.27%)"
-            }}
-        >
-            {advantagesBloc && advantagesBloc.map((adv, index) => {
-                return (
-                    <li key={index}>
+  return (
+      <ul className={` ${isBackground ? "flex flex-col gap-8 w-full h-auto text-transparent pointer-events-none select-none" : "absolute top-0 px-8 py-12  w-full h-[120%] flex flex-col gap-8 rounded-[50px] "} rounded-[50px] overflow-hidden`}
+          style={{
+            width: "min(485px, 90vw)",
+            background: currentTheme === "dark" ? "" : "linear-gradient(180deg, rgba(255, 255, 255, 0) 0%, rgba(15, 64, 146, 0.21) 70.27%)"
+          }}
+      >
+        {advantagesBloc && advantagesBloc.map((adv, index) => {
+          return (
+              <li key={index}>
                         <span
                             style={{
-                                fontFamily: 'Prompt',
-                                fontStyle: "normal",
-                                fontWeight: 600,
-                                fontSize: "24px",
-                                lineHeight: "36px",
-                                color: "#07ADF7",
+                              fontFamily: 'Prompt',
+                              fontStyle: "normal",
+                              fontWeight: 600,
+                              fontSize: "24px",
+                              lineHeight: "36px",
+                              color: "#07ADF7",
                             }}
-                        >{adv.name} :</span>
-                        <span
-                            className="block"
-                            style={{
-                                fontFamily: 'Outfit',
-                                fontStyle: "normal",
-                                fontWeight: 200,
-                                fontSize: "24px",
-                                lineHeight: "30px",
-                                color: "currentColor",
-                            }}
-                        >{adv.description}</span>
-                    </li>
-                )
-            })}
-        </ul>
-    )
+                        ><HighlightedText text={adv.titre}/> </span>
+                <span
+                    className="block"
+                    style={{
+                      fontFamily: 'Outfit',
+                      fontStyle: "normal",
+                      fontWeight: 200,
+                      fontSize: "24px",
+                      lineHeight: "30px",
+                      color: "currentColor",
+                    }}
+                ><HighlightedText text={adv.description}/> </span>
+              </li>
+          )
+        })}
+      </ul>
+  )
 }
--- src/components/strapi-components/Advantages.tsx
+++ src/components/strapi-components/Advantages.tsx
@@ -1,56 +1,58 @@
 "use client";
-import React, { useState } from "react";
+import React from "react";
 import HighlightedText from "../common/HighlightedText";
-import { BobButton } from "../ui/BobButton";
-import Image from "next/image";
-import { textMediaQueries } from "../home/hero/HeroContent";
 import CardBlur from "../home/advantages/CardBlur";
 import SVGB from "@/assets/svg/SVGB";
-import { useTheme } from "@/context/themeContext";
-import { StrapiPictureType } from "@/types/common";
-import { getStrapiMedia } from "@/utils/url.utils";
+import {useTheme} from "@/context/themeContext";
 import BlocQr from "../home/advantages/BlocQr";
-import { titleStyle } from "../home/style";
+import {titleStyle} from "../home/style";
+import {StrapiButton, StrapiPictureType} from "@/types/common";
 
 export interface AdvantagesBloc {
-    name: string;
-    description: string;
+  data: {
+    title: string;
+    avantages: {
+      titre: string;
+      description: string;
+    }[],
+    qr_code: StrapiPictureType,
+    image: StrapiPictureType,
+    button: StrapiButton
+  }
 }
 
-export default function Advantages() {
-    const { theme } = useTheme();
+export default function Advantages({data}: AdvantagesBloc) {
+  console.log(data)
+  const {theme} = useTheme();
 
-    const colors = {
-        color1: theme === "dark" ? "#166AF6" : "#e5f6ff",
-        color2: theme === "dark" ? "#00C9F7" : "#e7f0fe",
-    }
-    const title = `<h2>LES AVANTAGES</h2>`;
+  const colors = {
+    color1: theme === "dark" ? "#166AF6" : "#e5f6ff",
+    color2: theme === "dark" ? "#00C9F7" : "#e7f0fe",
+  }
 
-    const advantagesBloc: AdvantagesBloc[] = [
-        { name: "Enregistrez facilement vos prêts", description: "Ajoutez des objets ou des montants et suivez leur retour." },
-        { name: "Recevez des rappels intelligents", description: "Ne perdez plus un seul emprunt grâce aux notifications automatiques." },
-        { name: "Partagez avec vos amis", description: "Ajoutez des contacts et visualisez les échanges en un coup d'œil." }
-    ]
+  const QRText = " <h2>Scannez</h2>"
 
-    const QRText = " <h2>Scannez</h2>"
-    return (
-        <div className="relative container mx-auto px-8 flex flex-col gap-8 md:gap-16 text-[var(--foreground)]" data-section="advantages">
+  return (
+      <div className="relative container mx-auto px-8 flex flex-col gap-8 md:gap-16 text-[var(--foreground)]"
+           data-section="advantages">
 
-            <HighlightedText text={title} style={titleStyle} />
+        <HighlightedText text={data.title} style={titleStyle}/>
 
-            <div className="grid grid-cols-1 lg:grid-cols-2 justify-content-center place-items-center gap-8">
-                <div className="w-fit h-fit ">
-                    <CardBlur advantagesBloc={advantagesBloc} button={{ buttonText: "Téléchargez l'application", buttonLink: "" }} />
-                </div>
+        <div className="grid grid-cols-1 lg:grid-cols-2 justify-content-center place-items-center gap-8">
+          <div className="w-fit h-fit ">
+            <CardBlur advantagesBloc={data.avantages}
+                      button={{buttonText: data.button.text, buttonLink: data.button.url}}/>
+          </div>
 
-                <BlocQr QRText={QRText} />
-            </div>
+          <BlocQr QRText={QRText} QRImage={data.qr_code} MockupImage={data.image}/>
 
-            <div className="absolute top-0 left-0 -translate-x-1/4 -translate-y-8 w-3/4 h-3/4 -z-20">
-                <SVGB color1={colors.color1} color2={colors.color2} />
-            </div>
-        </div >
-    )
+        </div>
+
+        <div className="absolute top-0 left-0 -translate-x-1/4 -translate-y-8 w-3/4 h-3/4 -z-20">
+          <SVGB color1={colors.color1} color2={colors.color2}/>
+        </div>
+      </div>
+  )
 }
 
 
