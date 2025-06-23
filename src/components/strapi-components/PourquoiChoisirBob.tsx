323c47a851e19103aa463f2a42a82361cf9dca10
aceky <tafinasoa35@gmail.com>
2025-06-23 20:18:10 +0300

    Refactor layout for responsiveness in PourquoiChoisirBob.tsx
    Updated component layout to support better responsiveness, especially for smaller screens. Adjusted flex and grid behaviors, added conditional elements, and optimized image scaling for a more consistent design across device sizes.

--- src/components/strapi-components/PourquoiChoisirBob.tsx
+++ src/components/strapi-components/PourquoiChoisirBob.tsx
@@ -13,31 +13,35 @@ const PourquoiChoisirBob = ({data}: {
 
   return (
       <div
-          className="bg-gradient-to-b from-white/20 to-[#166AF6]/20 mt-32 px-20 text-white p-8 rounded-3xl flex items-center justify-between container mx-auto">
+          className="bg-gradient-to-b from-white/20 to-[#166AF6]/20 mt-32 px-20 text-white p-8 rounded-3xl flex flex-col md:flex-row items-center justify-between container mx-auto">
         <div className="w-10/12">
           <HighlightedText
               className="[font-family:'Prompt']  font-black text-3xl md:text-4xl lg:text-[64px] text-left uppercase max-w-lg text-[var(--foreground)]"
               text={data.titre}/>
-          <div className="grid grid-cols-4 gap-10 auto-rows-fr mt-10">
+          <div
+              className="border-l-2 lg:border-l-0 border-white flex flex-col gap-10 lg:grid lg:grid-cols-4 lg:gap-10 lg:auto-rows-fr mt-10">
             {data.avantages.map((avantage, index: number) => (
-                <div key={index} className="flex flex-col justify-between">
+                <div key={index}
+                     className="-ml-[9px] flex gap-4 items-start  lg:flex-col justify-start lg:justify-between">
+                  <div
+                      className="lg:hidden w-4 h-4 min-w-[1rem] min-h-[1rem] bg-white rounded-full self-start flex-shrink-0"></div>
                   <div>
                     <h3 className="text-lg font-semibold uppercase mb-4">{avantage.titre}</h3>
                     <HighlightedText className={"max-w-md"} text={avantage.description}/>
                   </div>
-                  <div className="w-5 h-5 bg-white rounded-full mt-4 self-start"></div>
+                  <div className="hidden lg:block w-5 h-5 bg-white rounded-full mt-4 self-start"></div>
                 </div>
             ))}
           </div>
-          <div className="-mt-[10px] w-full h-0.5 bg-white"></div>
+          <div className="hidden lg:block -mt-[10px] w-full h-0.5 bg-white"></div>
         </div>
-        <div className="w-2/12 relative">
+        <div className="w-full lg:w-2/12 relative">
           <Image
               src={getStrapiMedia(data.image.url) || ""}
               alt="Bob App on Phone"
               width={300}
               height={600}
-              className="scale-150"
+              className="lg:scale-150"
           />
         </div>
       </div>
