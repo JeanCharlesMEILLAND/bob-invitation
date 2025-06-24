3071e4669c1a1ef64c6bc9819bf3bf6a3e262566
aceky <tafinasoa35@gmail.com>
2025-06-24 11:17:01 +0300

    Add `react-device-detect` and optimize button handling
    Integrated the `react-device-detect` library to enhance device-specific button handling, especially for mobile devices. Refactored code to support multiple buttons and improved readability by adjusting prop structures and styles.

--- src/components/strapi-components/Advantages.tsx
+++ src/components/strapi-components/Advantages.tsx
@@ -3,10 +3,9 @@ import React from "react";
 import HighlightedText from "../common/HighlightedText";
 import CardBlur from "../home/advantages/CardBlur";
 import SVGB from "@/assets/svg/SVGB";
-import { useTheme } from "@/context/themeContext";
 import BlocQr from "../home/advantages/BlocQr";
-import { titleStyle } from "../home/style";
-import { StrapiButton, StrapiPictureType } from "@/types/common";
+import {titleStyle} from "../home/style";
+import {StrapiButton, StrapiPictureType} from "@/types/common";
 
 export interface AdvantagesBloc {
   data: {
@@ -18,36 +17,36 @@ export interface AdvantagesBloc {
     }[],
     qr_code: StrapiPictureType,
     image: StrapiPictureType,
-    button: StrapiButton
+    liens: StrapiButton[];
   }
 }
 
-export default function Advantages({ data }: AdvantagesBloc) {
-  const { title, qr_code, image, button, avantages } = data;
+export default function Advantages({data}: AdvantagesBloc) {
+  const {title, qr_code, image, liens, avantages} = data;
 
   const QRText = " <h2>Scannez</h2>"
 
 
   return (
-    <div className="relative container mx-auto px-8 flex flex-col gap-8 md:gap-16 text-[var(--foreground)]"
-      data-section="advantages">
+      <div className="relative container mx-auto px-8 flex flex-col gap-8 md:gap-16 text-[var(--foreground)]"
+           data-section="advantages">
 
-      <HighlightedText text={title} style={titleStyle} />
+        <HighlightedText text={title} style={titleStyle}/>
 
-      <div className="grid grid-cols-1 lg:grid-cols-2 justify-content-center place-items-center gap-8">
-        <div className="w-fit h-fit ">
-          <CardBlur advantagesBloc={avantages}
-            button={button} />
+        <div className="grid grid-cols-1 lg:grid-cols-2 justify-content-center place-items-center gap-8">
+          <div className="w-fit h-fit ">
+            <CardBlur advantagesBloc={avantages}
+                      button={liens}/>
+          </div>
+
+          <BlocQr QRText={QRText} QRImage={qr_code} MockupImage={image}/>
+
         </div>
 
-        <BlocQr QRText={QRText} QRImage={qr_code} MockupImage={image} />
-
+        <div className="absolute top-0 left-0 -translate-x-1/4 -translate-y-8 w-3/4 h-3/4 -z-20">
+          <SVGB/>
+        </div>
       </div>
-
-      <div className="absolute top-0 left-0 -translate-x-1/4 -translate-y-8 w-3/4 h-3/4 -z-20">
-        <SVGB />
-      </div>
-    </div>
   )
 }
 
